02bc9f866077d49299d58849173913e5fabdd841
Guillem Jover <guillem@hadrons.org>
2003-03-22 13:53:53 +0000

    examples/test.sh: Fix build base path and cleanup

--- doc/examples/test.sh
+++ doc/examples/test.sh
@@ -1,20 +1,30 @@
 #! /bin/sh
-fontpath=unix/:7101
+
+set -e
+
+fontpath="unix/:7101"
 #fontpath="inet/127.0.0.1:7101"
-make && echo done.
+fontpattern="-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+#fontpattern="-*-*-medium-r-normal-tt-*-*-*-*-*-*-iso8859-1"
+#fontencoding="--encoding windows-1251,iso8859-2,koi8-r"
+
+cd ../..
+make && echo Build done.
+
 sync
-time xfstt --once > lst &
-#time xfstt --once --encoding windows-1251,iso8859-2,koi8-r > lst &
+time src/xfstt --once $fontencoding > lst &
 sleep 1
-echo trying to connect via $fontpath
+
+echo Trying to connect via $fontpath
 xset +fp $fontpath
 xlsfonts > fonts.lst
-xfontsel -pattern "-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
-#xfontsel -pattern "-*-*-medium-r-normal-tt-*-*-*-*-*-*-iso8859-1"
+xfontsel -pattern $fontpattern
 #rxvt +sb -fn "TTM160_Courier New" -geometry 32x10
 xcoral -fn "TTM12_Courier New" -mfn "TTM16_Times New Roman" libfstt/raster_hints.cc &
 xcoral -bg white -fg black -fn "TTP18_Courier New" src/xfstt.cc
-echo disconnecting from $fontpath
+
+echo Disconnecting from $fontpath
 xset -fp $fontpath
-echo test complete.
+
+echo Test complete.
 
