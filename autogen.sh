fbffb25661753070a7e919eb09def4c0c72e3786
Guillem Jover <guillem@hadrons.org>
2005-04-27 23:34:12 +0000

    Indentation fixes

--- autogen.sh
+++ autogen.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 #
-# autogen.sh
+# $Id$
 #
-#	$Id$
+# Copyright (C) 2002 Guillem Jover <guillem@hadrons.org>
+# Some parts of this script come from Midnight Commander's autogen.sh
 #
-# Authors:	Guillem Jover <guillem@hadrons.org>
-#		Midnight Commander Authors
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# License:
-#
-#	Copyright (C) 2002 Guillem Jover
-#	Some parts of this script come from Midnight Commander's autogen.sh
-#
-#	This program is free software; you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation; either version 2 of the License, or
-#	(at your option) any later version.
-#
-# Requires:	gettext, automake, autoconf
+# Requires: gettext, automake, autoconf
 #
 
 PROJECT=xfstt
@@ -36,10 +29,10 @@ set -e
 srcdir=`dirname $0`
 if test -e $srcdir/$srcfile
 then
-	:
+  :
 else
-	echo "autogen.sh: No $PROJECT source dir" 2>&1
-	exit 1
+  echo "autogen.sh: No $PROJECT source dir" 2>&1
+  exit 1
 fi
 
 test -d config || mkdir config
@@ -57,30 +50,30 @@ gettext_ver=`$GETTEXTIZE --version | \
        '`
 
 if test $gettext_ver -lt 01038; then
-	echo "autogen.sh: Don't use gettext earlier than 0.10.38" 2>&1
-	exit 1
+  echo "autogen.sh: Don't use gettext earlier than 0.10.38" 2>&1
+  exit 1
 fi
 
 rm -rf intl
 if test $gettext_ver -ge 01100; then
-	if test $gettext_ver -lt 01105; then
-		echo "autogen.sh: Upgrade gettext to at least 0.11.5 or downgrade to 0.10.40" 2>&1
-		exit 1
-	fi
-	echo "-> $AUTOPOINT"
-	$AUTOPOINT --force || exit 1
+  if test $gettext_ver -lt 01105; then
+    echo "autogen.sh: Upgrade gettext to at least 0.11.5 or downgrade to 0.10.40" 2>&1
+    exit 1
+  fi
+  echo "-> $AUTOPOINT"
+  $AUTOPOINT --force || exit 1
 else
-	echo "-> $GETTEXTIZE"
-	echo "autogen.sh: Warning -- gettextize is not designed to be used automatically," 2>&1
-	echo "            so problems may arise. Upgrade to at least gettext 0.11.5" 2>&1
-	$GETTEXTIZE --intl --copy --force || exit 1
-	#echo "  + working around a nasty bug in gettext 0.11.5"
-	#sed -e 's,^\(AM_GNU_GETTEXT_VERSION([^)]\)$,\1),' < configure.ac > configure.ac.tmp
-	#mv -f configure.ac.tmp configure.ac
-	if test -e po/ChangeLog~; then
-		rm -f po/ChangeLog
-		mv po/ChangeLog~ po/ChangeLog
-	fi
+  echo "-> $GETTEXTIZE"
+  echo "autogen.sh: Warning -- gettextize is not designed to be used automatically," 2>&1
+  echo "            so problems may arise. Upgrade to at least gettext 0.11.5" 2>&1
+  $GETTEXTIZE --intl --copy --force || exit 1
+  #echo "  + working around a nasty bug in gettext 0.11.5"
+  #sed -e 's,^\(AM_GNU_GETTEXT_VERSION([^)]\)$,\1),' < configure.ac > configure.ac.tmp
+  #mv -f configure.ac.tmp configure.ac
+  if test -e po/ChangeLog~; then
+    rm -f po/ChangeLog
+    mv po/ChangeLog~ po/ChangeLog
+  fi
 fi
 
 echo "-> $ACLOCAL"
